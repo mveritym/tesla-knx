47358492d30ada8a1075e74d84f12c3f7a6ccca9
michaellennox <michaelctlennox@gmail.com>
2016-02-27 14:03:04 +0000

    adds patched teslams as dependency

--- server/routes.js
+++ server/routes.js
@@ -1,8 +1,12 @@
+var http = require("http");
 var express = require('express');
 var router = express.Router();
 
-router.post('', function(req, res) {
-  res.send('hello world')
+var tesla_api = 'https://private-anon-0ceef6f72-timdorr.apiary-mock.com/api/1/vehicles/1/';
+
+router.post('/tesla_climate', function(req, res) {
+  http.post(tesla_api + 'commands/')
+  res.send('hello world');
 });
 
 module.exports = router;
