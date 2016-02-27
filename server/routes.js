2f5c005de4f83c2f809ce0a82dfe02e856073a7f
michaellennox <michaelctlennox@gmail.com>
2016-02-27 14:58:06 +0000

    Adds the car end of setting house temp

--- server/routes.js
+++ server/routes.js
@@ -5,12 +5,16 @@ var router = express.Router();
 var teslams = require('teslams');
 var creds = require('../credentials.js');
 
-router.get('/tesla', function(req, res) {
+function setHouse(req, res) {
   teslams.get_vid({ email: creds.username, password: creds.password }, function (id) {
-  	teslams.get_vehicle_state(id , function (data) {
-  		res.json(data);
+  	teslams.get_climate_state(id , function (data) {
+      var temperature = data.driver_temp_setting;
+      //use temp to set house variables
+      res.json(data);
   	});
   });
-});
+}
+
+router.get('/set_house', setHouse);
 
 module.exports = router;
